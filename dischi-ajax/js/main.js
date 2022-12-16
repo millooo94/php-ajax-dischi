01fee453e85963da6620ff281f90d60ff77edbc4
millooo94 <camillomusmeciunict@gmail.com>
2022-12-16 16:19:32 +0100

    refactor all

--- dischi-ajax/js/main.js
+++ dischi-ajax/js/main.js
@@ -1,14 +1,34 @@
 new Vue({
     el: '#app',
     data: {
+        selected: '',
         disks: [],
+        genres: [],
         urlApi: location.href + 'api.php',
     },
     created() {
         axios.get(this.urlApi)
         .then(axiosResponse => {
-            console.log(axiosResponse);
             this.disks = axiosResponse.data
+            this.disks.forEach(album => {
+               while (!this.genres.includes(album.genre)) {
+                this.genres.push(album.genre)
+               }
+            });
+            console.log(this.genres)
         });
+    },
+    computed: {
+        onChange: function() {
+            axios.get(this.urlApi)
+            .then(axiosResponse => {
+                this.disks = axiosResponse.data.forEach(album => {
+                    if (album.genre == this.selected) {
+                        return album
+                    }
+                })
+            })
+
+        }
     }
 })
